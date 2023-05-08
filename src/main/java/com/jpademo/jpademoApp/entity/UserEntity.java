e88977a9e1d6a29638ed1a904086e3e1cf4d86f2
David0226 <sorwd2001@naver.com>
2023-05-08 22:47:45 +0900

    # Update demo

--- src/main/java/com/jpademo/jpademoApp/entity/UserEntity.java
+++ src/main/java/com/jpademo/jpademoApp/entity/UserEntity.java
@@ -16,7 +16,7 @@ public class UserEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 50, name = "user_name")
+    @Column(length = 50)
     private String username;
 
     @Column(nullable = false)
@@ -25,15 +25,15 @@ public class UserEntity {
     @Column(nullable = false, unique = true, length = 45)
     private String email;
 
-    @Column(length = 30, name = "phone_no")
-    private long phoneno;
+    @Column(length = 30)
+    private long phoneNo;
 
     @Column(name = "admin_yn")
-    private boolean adminyn;
+    private boolean adminYn;
 
     @CreatedDate
     @Column(name = "created_dt")
-    private LocalDateTime createddt;
+    private LocalDateTime createdDt;
 
     public static UserEntity of(String email, String password) {
         UserEntity userEntity = new UserEntity();
